29c882d8c444cebba43a6a43616171c8d31feb96
Zapata <zapata.devs@gmail.com>
2024-05-28 11:38:51 -0500

    Inputs Clear

--- navSections/contactUs.js
+++ navSections/contactUs.js
@@ -1,20 +1,27 @@
 const btn = document.getElementById('button');
+const from_name = document.getElementById("from_name");
+const message = document.getElementById("message");
+const email_id = document.getElementById("email_id");
 
 document.getElementById('form')
-    .addEventListener('submit', function(event) {
-    event.preventDefault();
+    .addEventListener('submit', function (event) {
+        event.preventDefault();
 
-btn.value = 'Sending...';
 
-const serviceID = 'default_service';
-const templateID = 'template_4x9u9xa';
+        btn.value = 'Sending...';
 
-emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-        btn.value = 'Send Email';
-        alert('Sent!');
-    }, (err) => {
-        btn.value = 'Send Email';
-        alert(JSON.stringify(err));
+        const serviceID = 'default_service';
+        const templateID = 'template_4x9u9xa';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                alert('Sent!');
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err));
+            });
+            from_name.value = "";
+            message.value = "";
+            email_id.value = "";
     });
-});
