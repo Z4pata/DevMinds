a6991fb4cc5fd76b463afb153da79c9fadef26db
Z4pata <zapata.devs@gmail.com>
2024-05-29 08:52:39 -0500

    fix script logo

--- globalScript/logo.js
+++ globalScript/logo.js
@@ -9,9 +9,9 @@ anchorLogo.addEventListener("click",()=> {
 function anchorLogoClick(){
     const age = localStorage.getItem("edad") 
     if (age == "+18"){
-        anchorLogo.href = "../adults/html/infoAdults.html";
+        anchorLogo.href = "/adults/html/infoAdults.html";
     }
     if (age == "-18"){
-        anchorLogo.href = "../Minors/html/infoMinors.html";
+        anchorLogo.href = "/Minors/html/infoMinors.html";
     }
 }
